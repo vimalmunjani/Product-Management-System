691ffa36ebcb8c8b25f73ed3d8c0ca42847b8a88
v.jitendra.munjani <v.jitendra.munjani@IGECO-DEV.AWSAPPS.COM>
2018-11-12 16:29:34 +0530

    added message module

---
+++ src/app/message/message.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MessageRoutingModule } from './message-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MessageRoutingModule
+  ]
+})
+export class MessageModule { }
