0350cd119e41de02c4943b7c8393d10efae8206b
v.jitendra.munjani <v.jitendra.munjani@IGECO-DEV.AWSAPPS.COM>
2018-11-15 11:08:56 +0530

    added prduct-list filter functionality

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -7,10 +7,11 @@ import { Product } from '../models/product.model';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
+  
   pageTitle: string = 'Product List';
   showImage: boolean = false;
-  filterText: string = 'i am a fliter text';
+  filterText: string;
+  filteredProducts: Product[]; 
   products: Product[] = [
     {
       productId:  1,
@@ -23,14 +24,37 @@ export class ProductListComponent implements OnInit {
       price: 1500,
       starRating: 3.2,
       imageUrl: "https://images10.newegg.com/ProductImageCompressAll1280/20-148-679-02.jpg"
+    },
+    {
+      productId:  2,
+      productName: "SSD",
+      productCode: "RAM-008-GB",
+      releaseDate: "March 19, 2016",
+      category : "CPU Parts",
+      description: "DDR3 RAM",
+      available: 5,
+      price: 1500,
+      starRating: 3.2,
+      imageUrl: "https://images10.newegg.com/ProductImageCompressAll1280/20-148-679-02.jpg"
     }
-  ]
+  ];
 
+  filterChanged(value: string) {
+    this.filteredProducts = (value) ? this.performFilter(value) : this.products
+  }
+
+  performFilter(value: string): Product[] {
+
+    return this.products.filter((p) => {
+      return  p.productName.trim().toLocaleLowerCase().indexOf(value.toLocaleLowerCase()) !== -1;
+    });
+
+  }
   
   constructor() { }
 
   ngOnInit() {
-
+    this.filteredProducts = this.products;
   }
 
   toggleImage() {
