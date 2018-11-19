cd62f53481fafa04a022aa8779a220e774ffa76a
v.jitendra.munjani <v.jitendra.munjani@IGECO-DEV.AWSAPPS.COM>
2018-11-19 11:47:02 +0530

    added HttpError Handler to product service

--- src/app/product/services/product.service.ts
+++ src/app/product/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, from } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, from, throwError } from 'rxjs';
+import { map, filter, catchError } from 'rxjs/operators';
 import { Product } from '../models/product.model';
 
 @Injectable({
@@ -14,13 +14,16 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Product[]> {
-     return this.http.get<Product[]>(this.productUrl);
+     return this.http.get<Product[]>(this.productUrl).pipe(
+       catchError(this.errorHandler)
+     );
   }
 
 
   getProductById(id: string): Observable<Product>{
 
     return this.http.get(this.productUrl).pipe(
+      catchError(this.errorHandler),
       map((products: Product[]) => products.find(p => p.productId === id))
     );
 
@@ -33,6 +36,20 @@ export class ProductService {
     //   console.log('product in service after filter', product);
     // });
     
+ }
+
+ private errorHandler(err: HttpErrorResponse) {
+ 
+    let errorMessage = '';
+    
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    
+    return throwError(errorMessage);
+
  }
 
 }
